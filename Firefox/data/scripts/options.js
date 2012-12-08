f94a8349fd27100a7d7b3232bcb63c91f20be610
sebadorn <seba_dorn@gmx.de>
2012-12-08 14:21:28 +0100

    Firefox: working.

---
+++ Firefox/data/scripts/options.js
@@ -0,0 +1,7 @@
+/* Dummy.
+ * Opera includes the files "includes/mle-codes.js" and "scripts/options.js" in its "options.html".
+ * However, that's not how it works in Firefox. But I want to re-use the same "options.html" file.
+ * So in Firefox in "includes/" and "scripts/" are just dummy files without actual content.
+ *
+ * The actual mle-codes.js and options.js are in the directory above.
+ */
