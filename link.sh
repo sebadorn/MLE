9ed5c8dc6a3ce0e8b32984651e758dd9c25d1480
sebadorn <sebadorn+git@posteo.de>
2025-03-17 19:51:27 +0100

    chore: remove linked files from git and only keep source in "common resources"

--- link.sh
+++ link.sh
@@ -10,11 +10,6 @@ TARGETS="$BASEDIR/common resources"
 
 # Check if target files exist
 
-if [ ! -f "$TARGETS/background.js" ]; then
-	echo " ! Missing target file: $TARGETS/background.js"
-	exit
-fi
-
 if [ ! -f "$TARGETS/mle-codes.js" ]; then
 	echo " ! Missing target file: $TARGETS/mle-codes.js"
 	exit
@@ -22,11 +17,6 @@ fi
 
 if [ ! -f "$TARGETS/my-little-emotebox.js" ]; then
 	echo " ! Missing target file: $TARGETS/my-little-emotebox.js"
-	exit
-fi
-
-if [ ! -f "$TARGETS/options-bootstrap.js" ]; then
-	echo " ! Missing target file: $TARGETS/options-bootstrap.js"
 	exit
 fi
 
@@ -45,28 +35,28 @@ if [ ! -f "$TARGETS/options.js" ]; then
 	exit
 fi
 
-if [ ! -f "$TARGETS/img/MLE_128.png" ]; then
-	echo " ! Missing target file: $TARGETS/img/MLE_128.png"
+if [ ! -f "$TARGETS/icons/MLE_128.png" ]; then
+	echo " ! Missing target file: $TARGETS/icons/MLE_128.png"
 	exit
 fi
 
-if [ ! -f "$TARGETS/img/MLE_16.png" ]; then
-	echo " ! Missing target file: $TARGETS/img/MLE_16.png"
+if [ ! -f "$TARGETS/icons/MLE_16.png" ]; then
+	echo " ! Missing target file: $TARGETS/icons/MLE_16.png"
 	exit
 fi
 
-if [ ! -f "$TARGETS/img/MLE_32.png" ]; then
-	echo " ! Missing target file: $TARGETS/img/MLE_32.png"
+if [ ! -f "$TARGETS/icons/MLE_32.png" ]; then
+	echo " ! Missing target file: $TARGETS/icons/MLE_32.png"
 	exit
 fi
 
-if [ ! -f "$TARGETS/img/MLE_48.png" ]; then
-	echo " ! Missing target file: $TARGETS/img/MLE_48.png"
+if [ ! -f "$TARGETS/icons/MLE_48.png" ]; then
+	echo " ! Missing target file: $TARGETS/icons/MLE_48.png"
 	exit
 fi
 
-if [ ! -f "$TARGETS/img/MLE_64.png" ]; then
-	echo " ! Missing target file: $TARGETS/img/MLE_64.png"
+if [ ! -f "$TARGETS/icons/MLE_64.png" ]; then
+	echo " ! Missing target file: $TARGETS/icons/MLE_64.png"
 	exit
 fi
 
@@ -74,30 +64,26 @@ fi
 # Linking
 
 # Chrome
-ln -f "$TARGETS/background.js" "$BASEDIR/Chrome/scripts/background.js"
 ln -f "$TARGETS/mle-codes.js" "$BASEDIR/Chrome/includes/mle-codes.js"
 ln -f "$TARGETS/my-little-emotebox.js" "$BASEDIR/Chrome/includes/my-little-emotebox.js"
 ln -f "$TARGETS/options.css" "$BASEDIR/Chrome/options.css"
 ln -f "$TARGETS/options.html" "$BASEDIR/Chrome/options.html"
-ln -f "$TARGETS/options-bootstrap.js" "$BASEDIR/Chrome/scripts/options-bootstrap.js"
 ln -f "$TARGETS/options.js" "$BASEDIR/Chrome/scripts/options.js"
-ln -f "$TARGETS/img/MLE_128.png" "$BASEDIR/Chrome/icons/MLE_128.png"
-ln -f "$TARGETS/img/MLE_16.png" "$BASEDIR/Chrome/icons/MLE_16.png"
-ln -f "$TARGETS/img/MLE_48.png" "$BASEDIR/Chrome/icons/MLE_48.png"
+ln -f "$TARGETS/icons/MLE_16.png" "$BASEDIR/Chrome/icons/MLE_16.png"
+ln -f "$TARGETS/icons/MLE_48.png" "$BASEDIR/Chrome/icons/MLE_48.png"
+ln -f "$TARGETS/icons/MLE_128.png" "$BASEDIR/Chrome/icons/MLE_128.png"
 echo ' - Done linking Chrome.'
 
-# Firefox WebExt
-ln -f "$TARGETS/background.js" "$BASEDIR/Firefox_WebExt/scripts/background.js"
+# Firefox
 ln -f "$TARGETS/mle-codes.js" "$BASEDIR/Firefox_WebExt/includes/mle-codes.js"
 ln -f "$TARGETS/my-little-emotebox.js" "$BASEDIR/Firefox_WebExt/includes/my-little-emotebox.js"
 ln -f "$TARGETS/options.css" "$BASEDIR/Firefox_WebExt/options.css"
 ln -f "$TARGETS/options.html" "$BASEDIR/Firefox_WebExt/options.html"
-ln -f "$TARGETS/options-bootstrap.js" "$BASEDIR/Firefox_WebExt/scripts/options-bootstrap.js"
 ln -f "$TARGETS/options.js" "$BASEDIR/Firefox_WebExt/scripts/options.js"
-ln -f "$TARGETS/img/MLE_128.png" "$BASEDIR/Firefox_WebExt/icons/MLE_128.png"
-ln -f "$TARGETS/img/MLE_16.png" "$BASEDIR/Firefox_WebExt/icons/MLE_16.png"
-ln -f "$TARGETS/img/MLE_48.png" "$BASEDIR/Firefox_WebExt/icons/MLE_48.png"
-echo ' - Done linking Firefox (WebExt).'
+ln -f "$TARGETS/icons/MLE_16.png" "$BASEDIR/Firefox_WebExt/icons/MLE_16.png"
+ln -f "$TARGETS/icons/MLE_48.png" "$BASEDIR/Firefox_WebExt/icons/MLE_48.png"
+ln -f "$TARGETS/icons/MLE_128.png" "$BASEDIR/Firefox_WebExt/icons/MLE_128.png"
+echo ' - Done linking Firefox.'
 
 echo ' All done.'
 
