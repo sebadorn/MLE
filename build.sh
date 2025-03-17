cced713a3077d9c4951f3f7ac2b74770872dde1b
sebadorn <sebadorn+git@posteo.de>
2025-03-17 18:58:36 +0100

    chore: remove support for Opera (pre Chrome/Blink engine era) and Firefox (old/legacy non-web-extension format)

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-MCCOY="/home/$USER/.mccoy/mccoy"
-CHROME="google-chrome"
+CHROME='google-chrome'
 
-FF_MLE_ID="mle@sebadorn.de"
-FF_MIN="38.0a1"
-FF_MAX="57.*"
+if [ ! "$(command -v $CHROME)" ]; then
+	CHROME='chromium'
+fi
 
 PROJECT_URL="https://sebadorn.de/mlp/mle"
 ABSOLUTE_PATH="/home/$USER/programming/My Little Emotebox"
 
 BROWSER="$1"
 VERSION="$2"
-
-export LD_LIBRARY_PATH="/home/$USER/.mccoy/xulrunner/"
 
 cd $(dirname "$0")
 
@@ -23,146 +20,83 @@ function set_version_and_url {
 	sed -i "s;%MLE_URL%;$PROJECT_URL;g" "$1"
 }
 
-
-function build_opera {
-	cd Opera/
-	cp config.xml ../config_tmp.xml
-
-	set_version_and_url config.xml
-	zip -r ../build/mle.oex *
-
-	mv ../config_tmp.xml config.xml
-	cd ../
-	cp server/updates-opera-template.xml build/updates-opera.xml
-
-	set_version_and_url build/updates-opera.xml
-}
-
-
 function build_chrome {
-	cp Chrome/manifest.json manifest_tmp.json
+	cp 'Chrome/manifest.json' 'manifest_tmp.json'
 
-	set_version_and_url Chrome/manifest.json
-	$CHROME --pack-extension="$ABSOLUTE_PATH/Chrome/" --pack-extension-key="$ABSOLUTE_PATH/build/chrome-private-key-mle.pem"
+	set_version_and_url 'Chrome/manifest.json'
+	$CHROME \
+		--pack-extension="$ABSOLUTE_PATH/Chrome/" \
+		--pack-extension-key="$ABSOLUTE_PATH/build/chrome-private-key-mle.pem"
 
-	mv manifest_tmp.json Chrome/manifest.json
-	mv Chrome.crx build/mle.crx
-	cp server/updates-chrome-template.xml build/updates-chrome.xml
+	mv 'manifest_tmp.json' 'Chrome/manifest.json'
+	mv 'Chrome.crx' 'build/mle.crx'
+	cp 'server/updates-chrome-template.xml' 'build/updates-chrome.xml'
 
-	set_version_and_url build/updates-chrome.xml
+	set_version_and_url 'build/updates-chrome.xml'
 }
 
 
 function build_chrome_store {
-	cd Chrome/
-	cp manifest.json ../manifest_tmp.json
+	cd 'Chrome/'
+	cp 'manifest.json' '../manifest_tmp.json'
 
-	set_version_and_url manifest.json
-	sed -i "s/\t\"update_url\".*//g" manifest.json
-	zip -r ../build/mle-chrome.zip *
+	set_version_and_url 'manifest.json'
+	sed -i 's/\t"update_url".*//g' 'manifest.json'
+	zip -r '../build/mle-chrome.zip' *
 
-	mv ../manifest_tmp.json manifest.json
-	cd ../
+	mv '../manifest_tmp.json' 'manifest.json'
+	cd '../'
 }
 
 
-function build_firefox_webext {
-	cd Firefox_WebExt/
-	cp manifest.json ../manifest_tmp.json
+function build_firefox {
+	cd 'Firefox_WebExt/'
+	cp 'manifest.json' '../manifest_tmp.json'
 
-	set_version_and_url manifest.json
-	zip -r -FS ../build/mle-ff-webext.zip *
+	set_version_and_url 'manifest.json'
+	zip -r -FS '../build/mle-ff-webext.zip' *
 
-	mv ../manifest_tmp.json manifest.json
-	cp ../server/updates-firefox-template.json ../build/updates-firefox.json
-}
-
-
-function build_firefox_legacy {
-	cd Firefox/
-	cp package.json ../package_tmp.json
-
-	local FF_API_USER=$(cat ../build/ff-sign-api-user.txt)
-	local FF_API_KEY=$(cat ../build/ff-sign-api-key.txt)
-
-	sed -i "s;%FF_MAX%;$FF_MAX;g" package.json
-	set_version_and_url package.json
-
-	jpm xpi
-	jpm sign --api-key ${FF_API_USER} --api-secret ${FF_API_KEY}
-
-	if [ -e "mle@sebadorn.de-${VERSION}.xpi" ]; then
-		mv "mle@sebadorn.de-${VERSION}.xpi" '../build/mle.xpi'
-		rm "${FF_MLE_ID}-${VERSION}.update.rdf"
-	elif [ -e "my_little_emotebox-${VERSION}-fx+an.xpi" ]; then
-		mv "my_little_emotebox-${VERSION}-fx+an.xpi" '../build/mle.xpi'
-		rm "${FF_MLE_ID}-${VERSION}.update.rdf"
-	elif [ -e "my_little_emotebox-${VERSION}-an+fx.xpi" ]; then
-		mv "my_little_emotebox-${VERSION}-an+fx.xpi" '../build/mle.xpi'
-		rm "${FF_MLE_ID}-${VERSION}.update.rdf"
-	fi
-
-	mv ../package_tmp.json package.json
-	cd ../
-
-	# Add updateHash to update.rdf and sign it for old
-	# installs that don't use the HTTPS updateLink.
-	local XPI_HASH=$(sha256sum build/mle.xpi | sed "s/ .*//g" -)
-
-	# Sign update RDF
-	cp "server/updates-firefox-template.rdf" "build/updates-firefox.rdf"
-	sed -i "s;%FF_MLE_ID%;$FF_MLE_ID;g" "build/updates-firefox.rdf"
-	sed -i "s;%FF_MIN%;$FF_MIN;g" "build/updates-firefox.rdf"
-	sed -i "s;%FF_MAX%;$FF_MAX;g" "build/updates-firefox.rdf"
-	sed -i "s;%XPI_HASH%;sha256:$XPI_HASH;g" "build/updates-firefox.rdf"
-	set_version_and_url "build/updates-firefox.rdf"
-
-	$MCCOY -signRDF "build/updates-firefox.rdf" -key "My Little Emotebox"
-
-	# Replace XPI hash in mle.js
-	build_page
+	mv '../manifest_tmp.json' 'manifest.json'
+	cp '../server/updates-firefox-template.json' '../build/updates-firefox.json'
 }
 
 
 function build_page {
-	cd server/
-	cp "mle-template.js" mle.js
-	set_version_and_url mle.js
-	cd ../
+	cd 'server/'
+	cp 'mle-template.js' 'mle.js'
+	set_version_and_url 'mle.js'
+	cd '../'
 }
 
 
-if [ $# -ge 1 ] && [ "$1" == "clean" ]; then
-	cd build
-	rm mle.xpi mle-ff-webext.zip "mle-unsigned.xpi" mle.crx mle.oex updates-*.xml updates-*.rdf
-	cd ../
+if [ $# -ge 1 ] && [ "$1" == 'clean' ]; then
+	cd 'build'
+	rm 'mle.xpi' 'mle-ff-webext.zip' "mle-unsigned.xpi" 'mle.crx' 'mle.oex' updates-*.xml updates-*.rdf
+	cd '../'
 	exit
 fi
 
 if [ $# -lt 2 ]; then
-	echo "Not enough arguments provided."
-	echo "First argument: all | opera | chrome | chrome_store | firefox | page | clean"
-	echo "Second argument: version"
+	echo 'Not enough arguments provided.'
+	echo 'First argument: all | chrome | chrome_store | firefox | page | clean'
+	echo 'Second argument: version'
 	exit
 fi
 
 
-if [ "$BROWSER" == "all" ]; then
-	build_opera
+if [ "$BROWSER" == 'all' ]; then
 	build_chrome
 	build_chrome_store
-	build_firefox_webext
-elif [ "$BROWSER" == "opera" ]; then
-	build_opera
-elif [ "$BROWSER" == "chrome" ]; then
+	build_firefox
+elif [ "$BROWSER" == 'chrome' ]; then
 	build_chrome
-elif [ "$BROWSER" == "chrome_store" ]; then
+elif [ "$BROWSER" == 'chrome_store' ]; then
 	build_chrome_store
-elif [ "$BROWSER" == "firefox" ]; then
-	build_firefox_webext
-elif [ "$BROWSER" == "page" ]; then
+elif [ "$BROWSER" == 'firefox' ]; then
+	build_firefox
+elif [ "$BROWSER" == 'page' ]; then
 	build_page
 fi
 
-echo " ---------- ---------- ---------- "
-echo " Done."
+echo ' ---------- ---------- ---------- '
+echo ' Done.'
--- link.sh
+++ link.sh
@@ -99,33 +99,6 @@ ln -f "$TARGETS/img/MLE_16.png" "$BASEDIR/Firefox_WebExt/icons/MLE_16.png"
 ln -f "$TARGETS/img/MLE_48.png" "$BASEDIR/Firefox_WebExt/icons/MLE_48.png"
 echo ' - Done linking Firefox (WebExt).'
 
-# Firefox
-ln -f "$TARGETS/background.js" "$BASEDIR/Firefox/lib/main.js"
-ln -f "$TARGETS/mle-codes.js" "$BASEDIR/Firefox/data/mle-codes.js"
-ln -f "$TARGETS/mle-codes.js" "$BASEDIR/Firefox/data/includes/mle-codes.js"
-ln -f "$TARGETS/my-little-emotebox.js" "$BASEDIR/Firefox/data/my-little-emotebox.js"
-ln -f "$TARGETS/options.css" "$BASEDIR/Firefox/data/options.css"
-ln -f "$TARGETS/options.html" "$BASEDIR/Firefox/data/options.html"
-ln -f "$TARGETS/options-bootstrap.js" "$BASEDIR/Firefox/data/scripts/options-bootstrap.js"
-ln -f "$TARGETS/options.js" "$BASEDIR/Firefox/data/options.js"
-ln -f "$TARGETS/options.js" "$BASEDIR/Firefox/data/scripts/options.js"
-ln -f "$TARGETS/img/MLE_16.png" "$BASEDIR/Firefox/data/icons/MLE_16.png"
-ln -f "$TARGETS/img/MLE_48.png" "$BASEDIR/Firefox/icon.png"
-ln -f "$TARGETS/img/MLE_64.png" "$BASEDIR/Firefox/icon64.png"
-echo ' - Done linking Firefox.'
-
-# Opera
-ln -f "$TARGETS/background.js" "$BASEDIR/Opera/scripts/background.js"
-ln -f "$TARGETS/mle-codes.js" "$BASEDIR/Opera/includes/mle-codes.js"
-ln -f "$TARGETS/my-little-emotebox.js" "$BASEDIR/Opera/includes/my-little-emotebox.js"
-ln -f "$TARGETS/options.css" "$BASEDIR/Opera/options.css"
-ln -f "$TARGETS/options.html" "$BASEDIR/Opera/options.html"
-ln -f "$TARGETS/options-bootstrap.js" "$BASEDIR/Opera/scripts/options-bootstrap.js"
-ln -f "$TARGETS/options.js" "$BASEDIR/Opera/scripts/options.js"
-ln -f "$TARGETS/img/MLE_16.png" "$BASEDIR/Opera/icons/MLE_16.png"
-ln -f "$TARGETS/img/MLE_64.png" "$BASEDIR/Opera/icons/MLE_64.png"
-echo ' - Done linking Opera.'
-
 echo ' All done.'
 
 # To check for correct linking you can first look up the inode ID of a file
