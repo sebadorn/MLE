c66bb699b52a4dbd53e14d76f0c235cb27236372
sebadorn <sebadorn+git@posteo.de>
2025-03-17 22:41:24 +0100

    chore: add ESLint config

---
+++ eslint.config.cjs
@@ -0,0 +1,46 @@
+const globals = require( 'globals' );
+const js = require( '@eslint/js' );
+
+
+module.exports = [
+	js.configs.recommended,
+	{
+		files: [
+			'*.js',
+			'**/*.js',
+		],
+		languageOptions: {
+			ecmaVersion: 2022,
+			globals: {
+				...globals.browser,
+				BG_TASK: true,
+				browser: false,
+				chrome: false,
+			},
+			sourceType: 'script',
+		},
+		ignores: [
+			'/_ignore/',
+			'/build/',
+			'/node_modules/',
+		],
+		rules: {
+			'no-unused-vars': [
+				'warn', {
+					argsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+				}
+			],
+			'no-var': ['warn'],
+			'quotes': ['warn', 'single'],
+		},
+	},
+	{
+		files: ['server/*.js'],
+		languageOptions: {
+			globals: {
+				InstallTrigger: true,
+			},
+		},
+	},
+];
