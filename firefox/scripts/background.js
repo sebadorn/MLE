5131e07f99dfba7328ab9138dc22205e0caf78d3
sebadorn <sebadorn+git@posteo.de>
2025-03-22 19:31:09 +0100

    change background script to service worker; include mle-codes.js in background.js

--- firefox/scripts/background.js
+++ firefox/scripts/background.js
@@ -1,6 +1,22 @@
 'use strict';
 
 
+// eslint-disable-next-line no-redeclare
+const BG_TASK = {
+	LOAD: 1,
+	SAVE_CONFIG: 2,
+	SAVE_EMOTES: 3,
+	RESET_CONFIG: 4,
+	RESET_EMOTES: 5,
+	OPEN_OPTIONS: 6,
+	UPDATE_EMOTES: 7,
+	UPDATE_LIST_ORDER: 8,
+	UPDATE_LIST_NAME: 9,
+	UPDATE_LIST_DELETE: 10,
+	UPDATE_CSS: 11,
+};
+
+
 // Set an individual User-Agent for our XMLHttpRequests.
 browser.webRequest.onBeforeSendHeaders.addListener(
 	/**
@@ -628,7 +644,7 @@ const Updater = {
 			return;
 		}
 
-		let url = this.xhrTargetsCSS[this.xhrProgress];
+		const url = this.xhrTargetsCSS[this.xhrProgress];
 
 		this.xhrCurrentTarget = this.xhrTargets[this.xhrProgress];
 		this.xhrProgress++;
